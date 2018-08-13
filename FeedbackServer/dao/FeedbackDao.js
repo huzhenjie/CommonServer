6d5f6ce13bdb8c79612cdb5af42533d0a21fb859
huzhenjie <huzhenjie.dev@gmail.com>
2018-08-13 11:21:48 +0800

    fix sql error

--- FeedbackServer/dao/FeedbackDao.js
+++ FeedbackServer/dao/FeedbackDao.js
@@ -4,7 +4,7 @@ function * addFeedback(appId, uid, title, ch, vc, vn, pt, contract, content) {
         yield Conn.query(
             'insert ignore into feedback set app_id=?,uid=?,title=?,ch=?,vc=?,vn=?,pt=?,contract=?,create_ts=?,content=?',
             {
-                replacements: [appId, uid, title, ch, vc, vn, contract, createTs, content],
+                replacements: [appId, uid, title, ch, vc, vn, pt, contract, createTs, content],
                 type: Sequelize.QueryTypes.INSERT
             });
     return insertId;
